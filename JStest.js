48d4264a9a85eb92cd6669c6ce392097c5fe2816
RuneSo <46044573+RuneSo@users.noreply.github.com>
2018-12-21 11:47:51 +0100

    Update JStest.js

--- JStest.js
+++ JStest.js
@@ -1,1 +1,45 @@
 alert("Javascript i JStest.js kjører!")
+
+<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/data.js"></script>
+$(function() {
+    var myChart = Highcharts.chart('figur1', {
+        series: [{
+            name: 'Ansatte',
+            data: [13258]
+        }, {
+            name: 'Årsverk',
+            data: [12521]
+        }, {
+            name: 'Stillinger',
+            data: [10326]
+        }, ],
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: "Figur 1: Antall stillinger, ansatte og årsverk i 1601 Trondheim kommune"
+        },
+        credits: {
+            enabled: true,
+            text: 'KS/PAI',
+            href: 'http://www.ks.no/pai'
+        },
+        xAxis: {
+            labels: {
+                enabled: false
+            }
+        },
+        yAxis: {
+            title: {
+                text: ''
+            },
+            labels: {
+                formatter: function() {
+                    return this.value;
+                }
+            }
+        }
+    });
+});
